47a8386e1ea50b430f111cb58e36f2aa43fb95a2
FerrariGuilherme <guilherme.ferrari@sp.gov.br>
2024-07-11 21:20:32 -0300

    11/07 v1.5

---
+++ Producao/Fiscal/Compilado/Auto Barcode - Fiscal 1.5/_internal/dependencias/apotamento_ambiente.py
@@ -0,0 +1,10 @@
+
+ambiente = "Fiscal"
+
+aponta_pdf = "\\\\10.16.96.144\\novo_auto_barcode\\pdf\\"
+
+caminho_json = "\\\\10.16.96.144\\novo_auto_barcode\\pdf\\JSON_Digitalizacao\\"
+
+aponta_database = "\\\\10.16.96.144\\novo_auto_barcode\\database\\database_producao.sqlite"
+
+
